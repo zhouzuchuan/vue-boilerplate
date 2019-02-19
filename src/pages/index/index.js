f7d26b94fd91e18e0322fd8da855d477817b866f
fezzc <zhouzuchuan@icloud.com>
2019-02-19 17:06:42 +0800

    添加 ant-design-vue

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -11,28 +11,13 @@ const { store, registerModel } = modelRedux.create()
 
 Vue.prototype.dispatch = store.dispatch
 
+// 注册model
 registerModel(appModel)
 
-// register global progress.
-const whiteList = ['/login', '/authredirect'] // 不重定向白名单
+// 路由统一处理
 router.beforeEach((to, from, next) => {
     nprogress.start() // 开启Progress
-    if (true) {
-        // 判断是否有token
-        if (to.path === '/login') {
-            next({ path: '/' })
-        } else {
-            next()
-        }
-    } else {
-        if (whiteList.indexOf(to.path) !== -1) {
-            // 在免登录白名单，直接进入
-            next()
-        } else {
-            next('/login') // 否则全部重定向到登录页
-            nprogress.done() // 在hash模式下 改变手动改变hash 重定向回来 不会触发afterEach 暂时hack方案 ps：history模式下无问题，可删除该行！
-        }
-    }
+    next()
 })
 
 router.afterEach(() => {

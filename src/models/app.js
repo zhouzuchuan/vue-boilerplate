f8e38058671b4c11f408422221cf8e294705ef76
fezzc <zhouzuchuan@icloud.com>
2019-05-21 17:55:10 +0800

    delete 注释

--- src/models/app.js
+++ src/models/app.js
@@ -1,39 +1,4 @@
-// import { switchMap, map } from 'rxjs/operators'
 
-// export default ({ serveGetPackageList }) => {
-//     return {
-//         namespace: 'app',
-//         state: {
-//             // 包列表[已安装的CLI插件, 生态系统]
-//             packageList: [[], []],
-//         },
-//         epics: {
-//             getList: epic$ =>
-//                 epic$.pipe(
-//                     switchMap(() => serveGetPackageList()),
-//                     map(({ data }) => {
-//                         return {
-//                             type: 'app/setState',
-//                             payload: {
-//                                 packageList: data.result,
-//                             },
-//                         }
-//                     }),
-//                 ),
-//         },
-//         reducers: {
-//             setState(state, { payload }) {
-//                 state.packageList = payload.packageList
-//                 console.log(state, payload)
-//                 return state
-//                 // return {
-//                 //     ...state,
-//                 //     ...payload,
-//                 // }
-//             },
-//         },
-//     }
-// }
 
 export default ({ serveGetPackageList }) => {
     return {

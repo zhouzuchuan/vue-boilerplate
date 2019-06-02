503969cf67a63e5c3cffc2eccb8e9a6a31e1b6ef
fezzc <zhouzuchuan@icloud.com>
2019-06-02 16:53:28 +0800

    feat: 添加storybook配置

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -4,33 +4,66 @@ import { storiesOf } from '@storybook/vue'
 import { action } from '@storybook/addon-actions'
 import { linkTo } from '@storybook/addon-links'
 
-import MyButton from './MyButton'
-import Welcome from './Welcome'
+import { text, boolean } from '@storybook/addon-knobs'
+import MyButton from './MyButton.vue'
 
-storiesOf('Welcome', module).add('to Storybook', () => ({
-    components: { Welcome },
-    template: '<welcome :showApp="action" />',
-    methods: { action: linkTo('Button') },
-}))
+import MD from './readme.md'
 
-storiesOf('Button', module)
-    .add('with text', () => ({
-        components: { MyButton },
-        template: '<my-button @click="action">Hello Button</my-button>',
-        methods: { action: action('clicked') },
-    }))
-    .add('with JSX', () => ({
-        components: { MyButton },
-        // eslint-disable-next-line no-unused-vars
-        render(h) {
-            return <my-button onClick={this.action}>With JSX</my-button>
+storiesOf('按钮', module)
+    .add(
+        '使用文本',
+        () => ({
+            components: { MyButton },
+            props: {
+                disabled: {
+                    default: boolean('Disabled', false),
+                },
+                label: {
+                    default: text('Text', 'Hello Storybook'),
+                },
+            },
+            template: `
+            <div>
+              <MyButton @click="action">Hello Button</MyButton>
+              <MyButton @click="action">Hello Button</MyButton>
+            </div>
+            `,
+            methods: { action: action('clicked') },
+        }),
+        {
+            info: {
+                // docsInPanel: false,
+            },
+            readme: {
+                content: '<!-- PROPS -->',
+                sidebar: MD,
+            },
         },
-        methods: { action: linkTo('clicked') },
-    }))
-    .add('with some emoji', () => ({
-        components: { MyButton },
-        template: '<my-button @click="action">😀 😎 👍 💯</my-button>',
-        methods: { action: action('clicked') },
-    }))
+    )
+    .add(
+        'with JSX',
+        () => ({
+            components: { MyButton },
+            // eslint-disable-next-line no-unused-vars
+            render(h) {
+                return <MyButton onClick={this.action}>With JSX</MyButton>
+            },
+            methods: { action: linkTo('clicked') },
+        }),
+        {
+            info: {},
+        },
+    )
+    .add(
+        'with some emoji',
+        () => ({
+            components: { MyButton },
+            template: '<my-button @click="action">😀 😎 👍 💯</my-button>',
+            methods: { action: action('clicked') },
+        }),
+        {
+            info: {},
+        },
+    )
 
 /* eslint-enable react/react-in-jsx-scope */
